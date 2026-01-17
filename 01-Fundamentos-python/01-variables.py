5c453af4be1f90a0c141bfead923af0c9d174cad
Eva Alonso Contreras <Eva.Alonso@proton.me>
2026-01-17 09:50:40 +0100

    variables

---
+++ 01-Fundamentos-python/01-variables.py
@@ -0,0 +1,15 @@
+name = "Eva"
+full_name = "Eva Alonso"
+
+print(name)
+print(full_name)
+
+#para ver dirección en memoria
+print(id(name))
+print(hex(id(name))) # en hexadecimal
+print(hex(id(full_name)))
+
+_private_name = "variable privada por convención"
+
+PI = 3.1416 # para constantes, ojo también es convención
+print(PI)
